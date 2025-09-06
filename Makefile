5618bc8a17cdc81b20df8921a4118f623261f8f7
Hasan Gursoy <hasangursoy@unplugdining.com>
2025-09-06 12:27:43 +0300

    update install script

--- Makefile
+++ Makefile
@@ -9,5 +9,14 @@ build:
 	GOOS=windows GOARCH=amd64 go build -o ./dist/gofred-windows-amd64.exe
 	GOOS=windows GOARCH=arm64 go build -o ./dist/gofred-windows-arm64.exe
 
+	cd ./dist && tar -czvf gofred-darwin-amd64.tar.gz gofred-darwin-amd64
+	cd ./dist && tar -czvf gofred-darwin-arm64.tar.gz gofred-darwin-arm64
+	cd ./dist && tar -czvf gofred-linux-amd64.tar.gz gofred-linux-amd64
+	cd ./dist && tar -czvf gofred-linux-arm64.tar.gz gofred-linux-arm64
+	cd ./dist && tar -czvf gofred-windows-amd64.tar.gz gofred-windows-amd64.exe
+	cd ./dist && tar -czvf gofred-windows-arm64.tar.gz gofred-windows-arm64.exe
+
+	rm ./dist/gofred-darwin-amd64 ./dist/gofred-darwin-arm64 ./dist/gofred-linux-amd64 ./dist/gofred-linux-arm64 ./dist/gofred-windows-amd64.exe ./dist/gofred-windows-arm64.exe
+
 clean:
 	rm -rf ./dist
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 CDN_URL="https://cdn.gofred.io"
 TEMP_DIR="/tmp"
@@ -34,6 +35,7 @@ fi
 
 if [ $OS == "Windows" ]; then
     OUTPUT_DIR="$HOME/AppData/Local/bin"
+    TEMP_DIR="$HOME/AppData/Local/Temp"
     if [ $ARCH == "arm64" ]; then
         OS="windows-arm64"
     else
@@ -46,14 +48,49 @@ if [ ! -d $OUTPUT_DIR ]; then
     mkdir -p $OUTPUT_DIR
 fi
 
-# Download the index.json file
-curl -L -o $TEMP_DIR/gofred.index.json $CDN_URL/cli/index.json
+# Check if tar is installed
+if ! command -v tar &> /dev/null; then
+    echo "tar could not be found. Installing it..."
+
+    if [ $OS == "darwin-arm64" ] || [ $OS == "darwin-amd64" ]; then
+        brew install tar
+    elif [ $OS == "linux-arm64" ] || [ $OS == "linux-amd64" ]; then
+        sudo apt-get install tar
+    elif [ $OS == "windows-arm64" ] || [ $OS == "windows-amd64" ]; then
+        winget install -e --id GnuWin32.Tar
+    fi
+fi
+
+# Check if jq is installed
+if ! command -v jq &> /dev/null; then
+    echo "jq could not be found. Installing it..."
+    
+    if [ $OS == "darwin-arm64" ] || [ $OS == "darwin-amd64" ]; then
+        brew install jq
+    elif [ $OS == "linux-arm64" ] || [ $OS == "linux-amd64" ]; then
+        sudo apt-get install jq
+    elif [ $OS == "windows-arm64" ] || [ $OS == "windows-amd64" ]; then
+        winget install -e --id stedolan.jq
+    fi
+fi
+
+# Download the index.json file silently
+curl -L -s -o  $TEMP_DIR/gofred.index.json $CDN_URL/cli/index.json
 
 # Parse the index.json file
 VERSION=$(cat $TEMP_DIR/gofred.index.json | jq -r '.version')
 
-# Download the gofred binary
-curl -L -o $OUTPUT_DIR/gofred $CDN_URL/cli/$VERSION/gofred-$OS
+# Download the gofred archive 
+curl -L -o $TEMP_DIR/gofred-$OS.tar.gz $CDN_URL/cli/$VERSION/gofred-$OS.tar.gz
+
+# Extract the gofred binary 
+tar -xf $TEMP_DIR/gofred-$OS.tar.gz -C $OUTPUT_DIR
+
+# Rename the binary to gofred
+mv $OUTPUT_DIR/gofred-$OS $OUTPUT_DIR/gofred
+
+# Remove the archive
+rm $TEMP_DIR/gofred-$OS.tar.gz
 
 # Make the file executable if OS is not Windows
 if [ $OS != "windows-arm64" ] && [ $OS != "windows-amd64" ]; then
